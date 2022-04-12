41c0b8b4f8757cb4146ee5726153439f681615a3
V-Svistunova <wika1706@gmail.com>
2022-04-12 18:04:45 +0300

    feat: add click to burger-menu and adaptive header-links

--- portfolio-2/app/js/main.js
+++ portfolio-2/app/js/main.js
@@ -1,3 +1,11 @@
 $(function () {
-
+  $('.header__btn-menu').on('click', function () {
+    $('.header__btn-menu').toggleClass('header__btn-menu--active'),
+    $('.header__nav').toggleClass('header__nav--active')
+    });  
+  
+    $('.nav-item').on('click', function () {
+      $('.header__btn-menu').removeClass('header__btn-menu--active'),
+      $('.header__nav').removeClass('header__nav--active')
+    });
 })
