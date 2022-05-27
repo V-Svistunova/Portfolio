9b36b1156d1070b3f22fa9a3c881ae2bd94901d4
V-Svistunova <wika1706@gmail.com>
2022-05-27 21:21:17 +0300

    feat: add files .js and img

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,22 @@
 import * as flsFunctions from "./modules/functions.js"
 
+
 flsFunctions.isWebp(); // для коректной работы пребразования файлов в webp формат
 
+$(function () {
+  $('.header__btn-menu').on('click', function () {
+    $('.header__btn-menu').toggleClass('header__btn-menu--active'),
+    $('.header__nav').toggleClass('header__nav--active')
+    });  
+  
+    $('.nav-item').on('click', function () {
+      $('.header__btn-menu').removeClass('header__btn-menu--active'),
+      $('.header__nav').removeClass('header__nav--active')
+    });
+
+    $('.portfolio__content').slick({
+      dots: true,
+      arrows: false,
+      autoplay: true,
+    });
+})
