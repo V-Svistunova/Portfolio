3ed88dee957547931cc7d3b256b67b1584ddbf29
V-Svistunova <wika1706@gmail.com>
2022-05-27 22:30:25 +0300

    fix: change icon

--- gulp/tasks/ftp.js
+++ gulp/tasks/ftp.js
@@ -1,4 +1,4 @@
-import { configFTP } from "../config/ftp";
+import { configFTP } from "../config/ftp.js";
 import vinylFTP from 'vinyl-ftp';
 import util from 'gulp-util';
 
--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@ import { images } from "./gulp/tasks/images.js";
 import { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js";
 import { svgSprive } from "./gulp/tasks/svgSprive.js";
 import { zip } from "./gulp/tasks/zip.js";
-import { ftp } from "./gulp/tasks/ftp";
+import { ftp } from "./gulp/tasks/ftp.js";
 
 
 // Наблюдатель за изменениями в файлах
