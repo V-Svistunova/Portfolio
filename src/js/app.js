5b73d6417b481ff45e394f758d10487b5ed2ab16
V-Svistunova <wika1706@gmail.com>
2022-07-07 23:27:14 +0300

    feat: add animation on scroll

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,10 @@ import * as flsFunctions from "./modules/functions.js";
 flsFunctions.isWebp(); // для коректной работы пребразования файлов в webp формат
 
 
+
 $(function () {
+
+  // open/close burger-menu 
   $('.header__btn-menu').on('click', function () {
     $('.header__btn-menu').toggleClass('header__btn-menu--active'),
     $('.header__nav').toggleClass('header__nav--active')
@@ -15,6 +18,9 @@ $(function () {
       $('.header__nav').removeClass('header__nav--active')
     });
 
+
+  
+  // responsive, client-side pagination
   $('.portfolio__spoiler-click').click(function(event) {
     $(this).toggleClass('active').next().slideToggle(300);
   });
@@ -24,15 +30,6 @@ $(function () {
       filter: '.webpage'
     }
   });
-
-    /*
-
-    $('.portfolio__content').slick({
-      dots: true,
-      arrows: false,
-      autoplay: true,
-    });
-    */
 });
 
 
@@ -53,5 +50,44 @@ for(let anchor of anchors) {
   })
 }
 
-// mixup
+
+
+// scroll animation
+
+const animItems = document.querySelectorAll('.anim-items');
+
+if (animItems.length > 0) {
+  window.addEventListener('scroll', animOnScroll);
+  function animOnScroll(params) {
+    for (let i=0; i < animItems.length; i++) {
+      const animItem = animItems[i];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight/animStart;
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight/animStart;
+      }
+
+      if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+        animItem.classList.add('active-animation');
+      } else {
+        if (!animItem.classList.contains('anim-no-hide')) {          
+          animItem.classList.remove('active-animation');
+        }
+      }
+    }
+    function offset(el) {
+      const rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
+  }
+
+  
+    animOnScroll();
+  
+}
 
